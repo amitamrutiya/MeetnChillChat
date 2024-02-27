728a1dce9fad322c95dd74807527351f053d42f0
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-02-27 14:48:01 +0530

    add comments in chat package
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

--- pkg/chat/client.go
+++ pkg/chat/client.go
@@ -10,78 +10,91 @@ import (
 
 const (
 	writeWait      = 10 * time.Second
-	pongWait       = 60 * time.Second
-	pingPeriod     = (pongWait * 9) / 10
+	pongWait       = 60 * time.Second    // 60s
+	pingPeriod     = (pongWait * 9) / 10 // 54s
 	maxMessageSize = 512
 )
 
+// Byte slices used for message processing
 var (
 	newline = []byte{'\n'}
 	space   = []byte{' '}
 )
 
-var upgrader = websocket.FastHTTPUpgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+// WebSocket upgrader configuration
+// var upgrader = websocket.FastHTTPUpgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// }
 
+// Client represents a chat client connected to the hub
 type Client struct {
-	Hub  *Hub
-	Conn *websocket.Conn
-	Send chan []byte
+	Hub  *Hub            // Reference to the hub this client is connected to
+	Conn *websocket.Conn // WebSocket connection of the client
+	Send chan []byte     // Channel for sending messages to the client
 }
 
+// readPump listens for messages from the WebSocket connection and sends them to the hub
 func (c *Client) readPump() {
 	defer func() {
-		c.Hub.unregister <- c
-		c.Conn.Close()
+		c.Hub.unregister <- c // Unregister client from the hub when done
+		c.Conn.Close()        // Close the WebSocket connection
 	}()
-	c.Conn.SetReadLimit(maxMessageSize)
-	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
-	c.Conn.SetPongHandler(func(string) error { c.Conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	c.Conn.SetReadLimit(maxMessageSize)              // Set maximum message size
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait)) // Set read deadline
+	c.Conn.SetPongHandler(func(string) error {       // Set pong handler to update read deadline
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
 	for {
-		_, message, err := c.Conn.ReadMessage()
+		_, message, err := c.Conn.ReadMessage() // Read message from WebSocket connection
 		if err != nil {
+			// Handle WebSocket read errors
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("error: %v", err)
 			}
+			log.Printf("other error: %v", err)
 			break
 		}
+		// Trim leading and trailing whitespace, replace newlines with spaces
 		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
-		c.Hub.broadcast <- message
+		c.Hub.broadcast <- message // Send message to the hub for broadcasting
 	}
 }
 
+// writePump listens for messages from the hub and writes them to the WebSocket connection
 func (c *Client) writePump() {
-	ticker := time.NewTicker(pingPeriod)
+	ticker := time.NewTicker(pingPeriod) // Create ticker for sending ping messages
 	defer func() {
 		ticker.Stop()
-		c.Conn.Close()
+		c.Conn.Close() // Close WebSocket connection when done
 	}()
 	for {
 		select {
-		case message, ok := <-c.Send:
-			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+		case message, ok := <-c.Send: // Receive message from the client's send channel
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait)) // Set write deadline
 			if !ok {
 				return
 			}
 
-			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			w, err := c.Conn.NextWriter(websocket.TextMessage) // Get writer for writing message
 			if err != nil {
 				return
 			}
-			w.Write(message)
+			w.Write(message) // Write message to the WebSocket connection
 
+			// Write any buffered messages to the WebSocket connection
 			n := len(c.Send)
 			for i := 0; i < n; i++ {
 				w.Write(newline)
 				w.Write(<-c.Send)
 			}
 
+			// Close the writer
 			if err := w.Close(); err != nil {
 				return
 			}
-		case <-ticker.C:
+		case <-ticker.C: // Periodically send ping messages to the client
 			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
 				return
@@ -90,10 +103,14 @@ func (c *Client) writePump() {
 	}
 }
 
+// PeerChatConn creates a new client and manages its lifecycle
 func PeerChatConn(c *websocket.Conn, hub *Hub) {
+	// Create a new client instance
 	client := &Client{Hub: hub, Conn: c, Send: make(chan []byte, 256)}
+	// Register the client with the hub
 	client.Hub.register <- client
 
+	// Start the client's write and read pumps concurrently
 	go client.writePump()
 	client.readPump()
 }
--- pkg/chat/hub.go
+++ pkg/chat/hub.go
@@ -1,36 +1,44 @@
 package chat
 
+// Hub represents a chat hub that manages clients
 type Hub struct {
-	clients    map[*Client]bool
-	broadcast  chan []byte
-	register   chan *Client
-	unregister chan *Client
+	clients    map[*Client]bool   // Map to store connected clients
+	broadcast  chan []byte        // Channel to broadcast messages to clients
+	register   chan *Client       // Channel to register new clients
+	unregister chan *Client       // Channel to unregister clients
 }
 
+// NewHub creates a new instance of Hub
 func NewHub() *Hub {
 	return &Hub{
-		broadcast:  make(chan []byte),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
-		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),  // Buffered channel for broadcasting messages
+		register:   make(chan *Client), // Channel for registering new clients
+		unregister: make(chan *Client), // Channel for unregistering clients
+		clients:    make(map[*Client]bool), // Map to store connected clients
 	}
 }
 
+// Run starts the chat hub to manage clients
 func (h *Hub) Run() {
 	for {
 		select {
 		case client := <-h.register:
+			// Register new client
 			h.clients[client] = true
 		case client := <-h.unregister:
+			// Unregister client
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.Send)
 			}
 		case message := <-h.broadcast:
+			// Broadcast message to all clients
 			for client := range h.clients {
 				select {
 				case client.Send <- message:
+					// Send message to client
 				default:
+					// If unable to send, close connection and unregister client
 					close(client.Send)
 					delete(h.clients, client)
 				}
